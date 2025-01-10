4dd17bdd75897a455137648c6bf7184c74857249
brandonyuan123 <brandonyuan05@gmail.com>
2025-01-09 19:24:47 -0600

    projects section outlined

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,56 +1,102 @@
 import './Home.css'
+
 import GitHubButton from './components/GitHubButton'
 import EmailButton from './components/EmailButton'
 import ResumeButton from './components/ResumeButton'
 import LinkedInButton from './components/LinkedInButton'
 
-import { Group, Container } from '@mantine/core';
+import NwcCard from './components/NwcCard'
+import RdcCard from './components/RdcCard'
+import SvmCard from './components/SvmCard'
 
+import { Group, Container, Grid, Text } from '@mantine/core';
 
-function Home () {
+function Home() {
   return (
     <>
-      <Container size='35rem'>
-        <h1>hi, i'm brandon yuan!</h1>
+      <Container size="60rem" style={{ marginTop: '15rem' }}>
         
-        <h2>
-          <Group gap="xs" style={{marginTop: '-1.5rem'}}> 
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/brandonyuann/">
-              <LinkedInButton />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/brandonyuanCS">
-              <GitHubButton />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="/resume.pdf">
-              <ResumeButton />
-            </a>
-            <a target="_blank" rel="noopener noreferrer" href="mailto:brandonyuan05@gmail.com">
-              <EmailButton />
-            </a>
-          </Group>
-        </h2>
+        
+        {/* Header and Picture Section */}
+        <div
+          style={{
+            display: 'flex',
+            gap: '2rem',
+            alignItems: 'flex-start', 
+            justifyContent: 'space-between'
+          }}
+        >
+          <div style={{ width: '35rem' }}>
+            <h1 style={{ marginTop: 0, marginBottom: 0 }}>hi, i'm brandon yuan!</h1>
 
-        <h3>i am...</h3>
-        <ul className="custom-list">
-          <li data-emoji="💻">an undergraduate CS student @ Texas A&M</li>
-          <li data-emoji="🤖">interested in web development & machine learning</li>
-          <li data-emoji="🎵">a fan of classical music, basketball, & video games</li>
-        </ul>
+            <ul className="custom-list">
+              <li data-emoji="🏫">I'm a CS student @ Texas A&M</li>
+              <li data-emoji="🤖">Interested in fullstack development, project management, & data science.</li>
+              <li data-emoji="💻">Lover of programming & passionate about learning</li>
+              <li data-emoji="💼">And looking for a 2025 summer internship!</li>
+            </ul>
 
-        <h3>previously, i've...</h3>
-        <ul className="custom-list">
-          <li data-emoji="📒">led & managed <a href="https://github.com/aggie-coding-club/notes-with-canvas"><em>Notes with Canvas</em></a> in the Aggie Coding Club</li>
-          <li data-emoji="👓">worked on a convolutional neural network in <em>Red Dead Convolution</em></li>
-        </ul>
 
-        <h3>i'm currently...</h3>
-        <ul className="custom-list">
-          <li data-emoji="🚧">a machine learning research assistant for: <em>Forecasting Absenteeism in Construction using Machine Learning</em> </li>
-          <li data-emoji="🎷">a project manager for <em>Spotify VibeMap</em></li>
-        </ul>
+
+            <h2 style={{ marginTop: "2.7rem" }}>
+              <Group gap="xs" style={{ marginTop: "-1.5rem" }}>
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/brandonyuann/">
+                  <LinkedInButton />
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/brandonyuanCS">
+                  <GitHubButton />
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="/resume.pdf">
+                  <ResumeButton />
+                </a>
+                <a target="_blank" rel="noopener noreferrer" href="mailto:brandonyuan05@gmail.com">
+                  <EmailButton />
+                </a>
+              </Group>
+            </h2>
+          </div>
+
+          <div style={{ width: '16rem', display: 'flex', justifyContent: 'center' }}>
+            <img src="pfp.jpg" alt="My picture" style={{ width: '100%', height: '100%', borderRadius: '50%' }} />
+          </div>
+        </div>
       </Container>
-    </>
+        
+
+      {/* Projects Section */}
+    <div style={{ marginTop: '15rem', backgroundColor: 'rgb(245, 245, 247)', paddingTop: '1rem', paddingBottom: '2rem'}}>
+      <Container size='60rem'>
+
+        <h2>Projects</h2>
+
+        <Grid grow>
+
+          <Grid.Col span={4}>
+            <NwcCard/>
+          </Grid.Col>
+            
+          <Grid.Col span={4}>
+            <SvmCard/>
+          </Grid.Col>
+
+          <Grid.Col span={4}>
+            <RdcCard/>
+          </Grid.Col>
+
+        </Grid>
+
+        <Text mt='3rem' ta='center' fw={600}>🌱 indicates work in progress</Text>
+
+      </Container>
+
+    </div>
+
+    {/* Skills Section */}
+    <Container size='60rem' style={{marginTop: '15rem'}}>
+      <h2>Skills</h2>
+    </Container>
+  </>
   );
 }
 
-export default Home
+export default Home;
