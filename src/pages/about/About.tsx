01a9b4bab0b5005f7cfbb3c36f33bbfd77d1df5f
brandonyuan123 <brandonyuan05@gmail.com>
2025-12-15 23:16:56 -0600

    updated abotu page content (yes, again)

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -10,69 +10,38 @@ export default function About() {
         📍 Austin, TX <br />
         🎓 CS @ Texas A&M <br />
       </p>
-      
-      {/* Two text paragraphs side by side */}
-      <Grid gutter="xl" className={`${styles.section} animate-fade-in-up-delay-2`}>
-        <Grid.Col span={{ base: 12, sm: 6 }}>
-          <p>
-            I'm a 3rd year CS student at TAMU who likes building software that feels 
-            reliable, intuitive, and just nice to use. I'm doing my best to become a better 
-            developer, one step at a time. Over this past year, I've spent a ton of time not only studying new technologies,
-            but also understanding how to write maintainable software, build systems end-to-end, 
-            and learn what users & customers actually look for.
-          </p>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, sm: 6 }}>
-          <p>
-            Outside of programming, I can be found at the gym, the basketball court, or my computer
-            still (sadly). I've recently started running, but am quite bad at it.
-          </p>
-          <p>
-            I like all kinds of music, and I hope to resume playing the trumpet & piano some day. Check
-            out my outdated <ExternalLink href='https://www.instagram.com/brandon.trumpet/'>music account</ExternalLink> at
-            your own risk. I also really enjoy cooking the same 10 things over and over.
-          </p>
-        </Grid.Col>
-      </Grid>
 
-      {/* Three columns with headers and lists */}
-      <Grid gutter="xl" className={`${styles.section} animate-fade-in-up-delay-3`}>
-        <Grid.Col span={{ base: 12, sm: 4 }}>
-          <div className={styles.columnSection}>
-            <h3>current goals</h3>
-            <div className={styles.divider}></div>
-            <p className={styles.list}>
-              finish & deploy canvas2calendar <br />
-              keep learning AWS & mongoDB <br />
-              experiment w/ framer & figma <br />
-              go to more hackathons! <br />
-              run a 10k
-            </p>
-          </div>
+      {/* Two text paragraphs side by side */}
+      <Grid gutter={75} className={`${styles.section} animate-fade-in-up-delay-2`}>
+        <Grid.Col span={{ base: 12, sm: 7 }}>
+          <p style={{ marginTop: 0 }}>
+            I'm a 3rd year CS student at TAMU who is determined to get really, really good at building software.
+            Much of my time is spent on personal projects. This past year, I've really dedicated a lot of time
+            towards studying design patterns & building systems that are reliable, scalable, and intuitive.
+          </p>
+          <p>
+            Outside of programming, I'll be at the gym, playing sports, playing video games, or running (although I am quite bad at it).
+            I also enjoy cooking the same 10 things over and over.
+          </p>
+          <p>
+            I'm planning on picking up the trumpet & piano again soon. Check
+            out my outdated <ExternalLink href='https://www.instagram.com/brandon.trumpet/'>music account</ExternalLink> at
+            your own risk.
+          </p>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, sm: 4 }}>
+        <Grid.Col span={{ base: 12, sm: 5 }}>
           <div className={styles.columnSection}>
-            <h3>to-do hobbies</h3>
+            <h3 style={{ color: '#222222' }}>current goals</h3>
             <div className={styles.divider}></div>
             <p className={styles.list}>
-              baking <br />
-              photography <br />
-              boxing <br />
-              violin <br />
-              art & graphic design
-            </p>
-          </div>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, sm: 4 }}>
-          <div className={styles.columnSection}>
-            <h3>my music h.o.f.</h3>
-            <div className={styles.divider}></div>
-            <p className={styles.list}>
-              rachmaninov - piano concerto 2 <br />
-              chopin - ballade no. 4 <br />
-              grouplove - tongue tied <br />
-              niki - dancing with the devil <br />
-              kendrick lamar - savior <br />
+              begin canvas2calendar beta testing <br />
+              go to more hackathons <br />
+              revive some old projects & ideas <br />
+              study AWS, MongoDB, Redis <br />
+              experiment with framer & figma <br />
+              study system & software design <br />
+              run a 10k <br />
+              get better at baking <br />
             </p>
           </div>
         </Grid.Col>
